0f608f7ea7ea4269a01da9ec9418accbb0fa2822
Ilya Sitnikov <sitnikovik@gmail.com>
2025-03-29 17:40:55 +0300

    Add Money interface to define methods for currency and units

---
+++ src/Money/Money.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the Eophantasy package.
+ *
+ * (c) Ilya Sitnikov <sitnikovik@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Represents a money object.
+ * 
+ * This interface defines the methods that a money class should implement.
+*/
+interface Money extends Stringable
+{
+    /**
+     * Returns the currency of the money object.
+     * 
+     * @return Currency
+     */
+    public function currency(): Currency;
+
+    /**
+     * Returns the number of units in the money object.
+     * 
+     * @return int
+     */
+    public function units(): int;
+
+    /**
+     * Returns the number of nanos in the money object.
+     * 
+     * @return int
+     */
+    public function nanos(): int;
+}
