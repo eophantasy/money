6007048da869be36d52b2e2a3026ff5e47677807
Ilya Sitnikov <sitnikovik@gmail.com>
2025-03-29 17:40:45 +0300

    Add Currency interface to define currency methods

---
+++ src/Currency/Currency.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Eophantasy package.
+ *
+ * (c) Ilya Sitnikov <sitnikovik@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Represents a currency for a money object.
+ * 
+ * This interface defines the methods that a currency class should implement.
+ */
+interface Currency
+{
+    /**
+     * Returns the currency code.
+     *
+     * @return string The currency code (e.g., "USD", "EUR").
+     */
+    public function code(): string;
+
+    /**
+     * Returns the currency symbol.
+     *
+     * @return string The currency symbol (e.g., "$", "€").
+     */
+    public function symbol(): string;
+}
