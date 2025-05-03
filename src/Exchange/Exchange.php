b5c0f1bc77a2895ac49675a5a6812bec95a58f5b
Ilya Sitnikov <sitnikovik@gmail.com>
2025-05-03 19:20:33 +0300

    Add Exchangale interface to define money exchange functionality

---
+++ src/Exchange/Exchange.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Eophantasy package.
+ *
+ * (c) Ilya Sitnikov <sitnikovik@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Represents an exchanger for money.
+ */
+interface Exchangale
+{
+    /**
+     * Exchanges the money object to another currency and returns the new money object.
+     */
+    public function exchange(): Money;
+}
