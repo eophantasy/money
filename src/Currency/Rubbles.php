487ff105bc9215caebe2f8eee569318ce01b3cee
Ilya Sitnikov <sitnikovik@gmail.com>
2025-05-03 19:16:25 +0300

    Add RubblesCurrency class to represent rubbles currency

---
+++ src/Currency/Rubbles.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the Eophantasy package.
+ *
+ * (c) Ilya Sitnikov <sitnikovik@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Represents rubbles currency for a money object.
+ */
+final class RubblesCurrency implements Currency
+{
+    /**
+     * The currency code (e.g., "RUB").
+     * 
+     * @var string 
+     */
+    private string $code;
+
+    /**
+     * The currency symbol (e.g., "руб.").
+     * 
+     * @var string 
+     */
+    private string $symbol;
+
+    /**
+     * Creates a new instance of the RubblesCurrency class.
+     */
+    public function __construct()
+    {
+        $this->code = "RUB";
+        $this->symbol = "руб.";
+    }
+
+    /**
+     * Returns the currency code.
+     *
+     * @return string The currency code (e.g., "RUB").
+     */
+    public function code(): string
+    {
+        return $this->code;
+    }
+
+    /**
+     * Returns the currency symbol.
+     *
+     * @return string The currency symbol (e.g., "руб.").
+     */
+    public function symbol(): string
+    {
+        return $this->symbol;
+    }
+}
